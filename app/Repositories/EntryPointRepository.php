6b43c977b0a9d04716bc14b38bde276a19548528
Jonas Ivy <jonasivyimperial@gmail.com>
2022-08-13 14:10:42 +0800

    :fire: Implemented required for get and post entry endpoint.

--- app/Repositories/EntryPointRepository.php
+++ app/Repositories/EntryPointRepository.php
@@ -16,4 +16,50 @@ class EntryPointRepository extends Repository
     {
         $this->model = $model;
     }
+
+    /**
+     * Get one resources.
+     *
+     * @param int $x
+     * @param int $y
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function isExists(int $x, int $y)
+    {
+        return $this->model
+            ->refresh()
+            ->remember(config('cache.retention'))
+            ->where([
+                'x_axis' => $x,
+                'y_axis' => $y,
+            ])
+            ->exists();
+    }
+
+    /**
+     * Get all entry points.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function getAllEntryPoints()
+    {
+        return $this->model
+            ->refresh()
+            ->get();
+    }
+
+    /**
+     * Save new entry point to database.
+     *
+     * @param int $x
+     * @param int $y
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function saveEntryPoint(int $x, int $y)
+    {
+        return $this->model->create([
+            'x_axis' => $x,
+            'y_axis' => $y,
+        ]);
+    }
 }
