12eb3d04f394819f62ba3ec9e72eadeb2f49c20e
Jonas Ivy <jonasivyimperial@gmail.com>
2022-08-17 21:47:03 +0800

    :fire: Added condition to rate limit only if production.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -45,8 +45,10 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function configureRateLimiting()
     {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
+        if ('production' === env('APP_ENV')) {
+            RateLimiter::for('api', function (Request $request) {
+                return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            });
+        }
     }
 }
